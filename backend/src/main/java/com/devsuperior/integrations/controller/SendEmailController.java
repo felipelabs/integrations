dda3f2a3a8ecd8a9f0024e3f73d32e598e0f7396
Felipe Neves <felipelabs@gmail.com>
2024-06-03 22:00:51 -0300

    Add service and Sendgrid dependence

--- backend/src/main/java/com/devsuperior/integrations/controller/SendEmailController.java
+++ backend/src/main/java/com/devsuperior/integrations/controller/SendEmailController.java
@@ -1,6 +1,8 @@
 package com.devsuperior.integrations.controller;
 
 import com.devsuperior.integrations.dto.SendEmailDTO;
+import com.devsuperior.integrations.service.SendEmailService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,8 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/emails")
 public class SendEmailController {
 
+    @Autowired
+    SendEmailService sendEmailService;
+
     @PostMapping
     public ResponseEntity<Void> sendEmail(@RequestBody SendEmailDTO sendEmailDTO){
+        sendEmailService.sendEmail(sendEmailDTO);
         return ResponseEntity.noContent().build();
     }
 }
---
+++ backend/src/main/java/com/devsuperior/integrations/service/SendEmailService.java
@@ -0,0 +1,41 @@
+package com.devsuperior.integrations.service;
+
+import com.devsuperior.integrations.dto.SendEmailDTO;
+import com.sendgrid.Method;
+import com.sendgrid.Request;
+import com.sendgrid.Response;
+import com.sendgrid.SendGrid;
+import com.sendgrid.helpers.mail.Mail;
+import com.sendgrid.helpers.mail.objects.Content;
+import com.sendgrid.helpers.mail.objects.Email;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+
+@Service
+public class SendEmailService {
+
+    @Autowired
+    private SendGrid sendGrid;
+
+    public void sendEmail(SendEmailDTO sendEmailDTO){
+
+        Email from = new Email(sendEmailDTO.getFromEmail(), sendEmailDTO.getFromName());
+        Email to = new Email(sendEmailDTO.getTo());
+        Content content = new Content(sendEmailDTO.getContentType(), sendEmailDTO.getBody());
+        Mail mail = new Mail(from, sendEmailDTO.getSubject(), to, content);
+
+        Request request = new Request();
+
+        try {
+            request.setMethod(Method.POST);
+            request.setEndpoint("mail/send");
+            request.setBody(mail.build());
+            Response response = sendGrid.api(request);
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
+    }
+}
